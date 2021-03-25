1e1aac6445ec19987cd7dc7c5ab9b32a2e633923
Mina Nabil <mina9492@hotmail.com>
2021-03-25 06:44:03 +0200

    Making dark default

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -441,7 +441,7 @@
             <div class="r-panel-body">
                 <ul id="themecolors" class="m-t-20">
                     <li><b>With Light sidebar</b></li>
-                    <li><a href="javascript:void(0)" data-skin="skin-default" class="default-theme working">1</a></li>
+                    <li><a href="javascript:void(0)" data-skin="skin-default" class="default-theme">1</a></li>
                     <li><a href="javascript:void(0)" data-skin="skin-green" class="green-theme">2</a></li>
                     <li><a href="javascript:void(0)" data-skin="skin-red" class="red-theme">3</a></li>
                     <li><a href="javascript:void(0)" data-skin="skin-blue" class="blue-theme">4</a></li>
@@ -452,7 +452,7 @@
                     <li><a href="javascript:void(0)" data-skin="skin-green-dark" class="green-dark-theme">8</a></li>
                     <li><a href="javascript:void(0)" data-skin="skin-red-dark" class="red-dark-theme">9</a>
                     </li>
-                    <li><a href="javascript:void(0)" data-skin="skin-blue-dark" class="blue-dark-theme">10</a></li>
+                    <li><a href="javascript:void(0)" data-skin="skin-blue-dark" class="blue-dark-theme working">10</a></li>
                     <li><a href="javascript:void(0)" data-skin="skin-purple-dark" class="purple-dark-theme">11</a></li>
                     <li><a href="javascript:void(0)" data-skin="skin-megna-dark" class="megna-dark-theme ">12</a></li>
                 </ul>
